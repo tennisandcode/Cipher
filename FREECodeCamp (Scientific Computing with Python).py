a91cb0412a6e290c9bb1422923e9b3507c4e8766
tennisandcode <154731536+tennisandcode@users.noreply.github.com>
2023-12-26 19:25:47 +0800

    Step 87

--- FREECodeCamp (Scientific Computing with Python).py
+++ FREECodeCamp (Scientific Computing with Python).py
@@ -1,17 +1,35 @@
-text = 'Hello Zaira'
-shift = 3
+text = 'mrttaqrhknsw ih puggrur'
+custom_key = 'python'
 
-def caesar():
+def vigenere(message, key, direction=1):
+    key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+    final_message = ''
 
-    for char in text.lower():
-        if char == ' ':
-            encrypted_text += char
-        else:
+    for char in message.lower():
+
+        # Append any non-letter character to the message
+        if not char.isalpha():
+            final_message += char
+        else:        
+            # Find the right key character to encode/decode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + shift) % len(alphabet)
-            encrypted_text += alphabet[new_index]
-    print('plain text:', text)
-    print('encrypted text:', encrypted_text)
-caesar()
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+print('Encrypted text: ' + text)
+print('Key: ' + custom_key)
+decryption = decrypt(text, custom_key)
+print('Decrypted text: ' + decryption)
